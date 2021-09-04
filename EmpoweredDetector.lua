03c0e5715bf29978a47b857069dd5f5089d1e707
omsheal <90083715+omsheal@users.noreply.github.com>
2021-09-04 14:18:49 -0700

    Update EmpoweredDetector.lua
    Update help command

--- EmpoweredDetector.lua
+++ EmpoweredDetector.lua
@@ -10,27 +10,41 @@ local empoweredSpells = {
 	[357861] = true, -- Activate Empowerment
 }
 
+-- Help command
+local cmdColor = "|cfffff569"
+local cmdHelp = {
+	"|cffff4500Empowered|r|cff00ffffDetector|r: Slash Commands:",
+	cmdColor.."/ed|r - print status",
+	cmdColor.."/ed help|r - list of commands",
+	cmdColor.."/ed on|r - enable EmpoweredDetector",
+	cmdColor.."/ed off|r - disable EmpoweredDetector",
+	cmdColor.."/ed group|r - announce to group",
+	cmdColor.."/ed say|r - announce to /say",
+	cmdColor.."/ed yell|r - announce to /yell",
+	cmdColor.."/ed self|r - annouce to self",
+	cmdColor.."/ed test|r - test the announcer",
+}
+
 -- Configuration
 local function setting(cmd)
 	cmd=string.upper(cmd)
-	if cmd == 'ON' or cmd == 'OFF' then
-		EmpoweredDetector_Enabled = (cmd == 'ON')
+	if cmd == "ON" or cmd == "OFF" then
+		EmpoweredDetector_Enabled = (cmd == "ON")
 		if EmpoweredDetector_Enabled then
 			EmpoweredDetectorFrame:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 		else
 			EmpoweredDetectorFrame:UnregisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 		end
 		print("Empowered Detector: " .. (EmpoweredDetector_Enabled and "|cff85ff85On|r" or "|cffff0000Off|r") .. ".")
-		
-	elseif cmd == 'SAY' or cmd == 'GROUP' or cmd == 'SELF' or cmd == 'YELL' then
+	elseif cmd == "SAY" or cmd == "GROUP" or cmd == "SELF" or cmd == "YELL" then
 		EmpoweredDetector_Mode = cmd
 		print("Empowered Detector will announce to |cffff9c00"..EmpoweredDetector_Mode.."|r.")
-		
-	elseif cmd == 'TEST' then
+	elseif cmd == "TEST" then
 		EmpoweredDetectorFrame:Announce("[Test] Empowered Detector Announcer.")
-		
+	elseif cmd == "HELP" then
+		print(table.concat(cmdHelp, "|n"))
 	else
-		print("Empowered Detector Status: "..(EmpoweredDetector_Enabled and "|cff85ff85On|r" or "|cffff0000Off|r").."|r and announcing to: |cff1cb619"..EmpoweredDetector_Mode.."|r.\nCommands: {on|off|group|say|yell|self|test}")
+		print("Empowered Detector Status: "..(EmpoweredDetector_Enabled and "|cff85ff85On|r" or "|cffff0000Off|r").."|r and announcing to: |cff1cb619"..EmpoweredDetector_Mode.."|r.")
 	end
 end
 
@@ -40,7 +54,8 @@ SlashCmdList["ED"] = setting;
 SLASH_EMPOWERDETECTOR1 = "/empowerdetector"
 SLASH_ED1 = "/ed"
 
-EmpoweredDetectorFrame:SetScript('OnEvent', function( self, event, ... )
+-- Start of EmpoweredDetectorFrame Detection
+EmpoweredDetectorFrame:SetScript("OnEvent", function( self, event, ... )
 	self[event]( self, ... )
 end)
 
@@ -76,6 +91,7 @@ function EmpoweredDetectorFrame:UNIT_SPELLCAST_SUCCEEDED(unitTarget, _, spellID)
 		end
 	end
 end
+-- End of EmpoweredDetectorFrame Detection
 
 -- Announcer Redirector
 function EmpoweredDetectorFrame:Announce(msg)
